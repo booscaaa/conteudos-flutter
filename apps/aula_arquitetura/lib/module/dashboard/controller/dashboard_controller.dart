40b580cf8981d77488b745276123e817bdf62381
booscaaa <boscardinvinicius@gmail.com>
2025-09-25 08:36:07 -0300

    ✨ feat(controller): adiciona funcionalidade de inserir pessoa e controle de loading

--- apps/aula_arquitetura/lib/module/dashboard/controller/dashboard_controller.dart
+++ apps/aula_arquitetura/lib/module/dashboard/controller/dashboard_controller.dart
@@ -1,18 +1,32 @@
 import 'package:aula_arquitetura/module/dashboard/core/domain/model/pessoa.dart';
+import 'package:aula_arquitetura/module/dashboard/core/domain/usecase/inserir_pessoa_usecase.dart';
 import 'package:aula_arquitetura/module/dashboard/core/domain/usecase/listar_pessoas_usecase.dart';
 
 class DashboardController {
+  bool loading = false;
   List<Pessoa> pessoas = [];
   Function setState;
   final ListarPessoasUsecase listarPessoasUseCase;
+  final InserirPessoaUsecase inserirPessoaUsecase;
 
   DashboardController({
     required this.listarPessoasUseCase,
+    required this.inserirPessoaUsecase,
     required this.setState,
   });
 
   Future listarPessoas() async {
-    pessoas = await listarPessoasUseCase();
+    loading = true;
     setState(() {});
+    pessoas = await listarPessoasUseCase();
+    loading = false;
+    setState(() {});
+  }
+
+  adicionarPessoa(Pessoa pessoa) async {
+    loading = true;
+    setState(() {});
+    await inserirPessoaUsecase(pessoa);
+    await listarPessoas();
   }
 }
