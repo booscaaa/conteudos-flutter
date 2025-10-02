574c82f92642b205af4f5efc9a2630e6a06ac4aa
booscaaa <boscardinvinicius@gmail.com>
2025-10-01 22:12:43 -0300

    ♻️ refactor(repositório): adapta repositórios para implementar contratos

--- apps/aula_arquitetura/lib/module/dashboard/data/repository/inserir_pessoa_repository.dart
+++ apps/aula_arquitetura/lib/module/dashboard/data/repository/inserir_pessoa_repository.dart
@@ -1,11 +1,15 @@
 import 'package:aula_arquitetura/module/dashboard/core/domain/model/pessoa.dart';
+import 'package:aula_arquitetura/module/dashboard/core/domain/contract/inserir_pessoa_repository.dart';
 import 'package:aula_arquitetura/util/database.dart';
+import 'package:injectable/injectable.dart';
 
-class InserirPessoaRepository {
+@Injectable(as: InserirPessoaRepository)
+class InserirPessoaRepositoryImpl implements InserirPessoaRepository {
   final Database database;
 
-  InserirPessoaRepository({required this.database});
+  InserirPessoaRepositoryImpl({required this.database});
 
+  @override
   Future<void> call(Pessoa pessoa) async {
     await Future.delayed(Duration(seconds: 1));
     database.insert(pessoa);
--- apps/aula_arquitetura/lib/module/dashboard/data/repository/listar_pessoas_repository.dart
+++ apps/aula_arquitetura/lib/module/dashboard/data/repository/listar_pessoas_repository.dart
@@ -1,11 +1,15 @@
 import 'package:aula_arquitetura/module/dashboard/core/domain/model/pessoa.dart';
+import 'package:aula_arquitetura/module/dashboard/core/domain/contract/listar_pessoas_repository.dart';
 import 'package:aula_arquitetura/util/database.dart';
+import 'package:injectable/injectable.dart';
 
-class ListarPessoasRepository {
+@Injectable(as: ListarPessoasRepository)
+class ListarPessoasRepositoryImpl implements ListarPessoasRepository {
   final Database database;
 
-  ListarPessoasRepository({required this.database});
+  ListarPessoasRepositoryImpl({required this.database});
 
+  @override
   Future<List<Pessoa>> call() async {
     await Future.delayed(Duration(seconds: 1));
     return database.select();
