4c5069e289bd29639d404b78bdc0fbf5cba2e706
booscaaa <boscardinvinicius@gmail.com>
2025-10-01 22:12:53 -0300

    ♻️ refactor(controller): atualiza controller e remove arquivo de DI obsoleto

--- apps/aula_arquitetura/lib/module/dashboard/controller/dashboard_controller.dart
+++ apps/aula_arquitetura/lib/module/dashboard/controller/dashboard_controller.dart
@@ -1,31 +1,30 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:aula_arquitetura/module/dashboard/core/domain/model/pessoa.dart';
 import 'package:aula_arquitetura/module/dashboard/core/domain/usecase/inserir_pessoa_usecase.dart';
 import 'package:aula_arquitetura/module/dashboard/core/domain/usecase/listar_pessoas_usecase.dart';
+import 'package:aula_arquitetura/module/dashboard/state/dashboard_state.dart';
+import 'package:injectable/injectable.dart';
 
-class DashboardController {
-  bool loading = false;
-  List<Pessoa> pessoas = [];
-  Function setState;
+@injectable
+class DashboardController extends Cubit<DashboardState> {
   final ListarPessoasUsecase listarPessoasUseCase;
   final InserirPessoaUsecase inserirPessoaUsecase;
 
   DashboardController({
     required this.listarPessoasUseCase,
     required this.inserirPessoaUsecase,
-    required this.setState,
-  });
-
-  Future listarPessoas() async {
-    loading = true;
-    setState(() {});
-    pessoas = await listarPessoasUseCase();
-    loading = false;
-    setState(() {});
+  }) : super(DashboardInitial()) {
+    listarPessoas(); // Carrega automaticamente ao criar o cubit
   }
 
-  adicionarPessoa(Pessoa pessoa) async {
-    loading = true;
-    setState(() {});
+  Future<void> listarPessoas() async {
+    emit(DashboardLoading());
+    final pessoas = await listarPessoasUseCase();
+    emit(DashboardLoaded(pessoas));
+  }
+
+  Future<void> adicionarPessoa(Pessoa pessoa) async {
+    emit(DashboardLoading());
     await inserirPessoaUsecase(pessoa);
     await listarPessoas();
   }
