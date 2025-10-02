b8a34781d150a223ac5ec2d37c1c4ab51a95c7c7
booscaaa <boscardinvinicius@gmail.com>
2025-10-01 22:13:04 -0300

    ♻️ refactor(view): atualiza view para utilizar novo gerenciamento de estado

--- apps/aula_arquitetura/lib/module/dashboard/view/dashboard.dart
+++ apps/aula_arquitetura/lib/module/dashboard/view/dashboard.dart
@@ -1,53 +1,53 @@
+import 'package:aula_arquitetura/core/injection.dart';
 import 'package:aula_arquitetura/module/dashboard/controller/dashboard_controller.dart';
+import 'package:aula_arquitetura/module/dashboard/state/dashboard_state.dart';
 import 'package:aula_arquitetura/module/dashboard/core/domain/model/pessoa.dart';
-import 'package:aula_arquitetura/module/dashboard/core/domain/usecase/listar_pessoas_usecase.dart';
-import 'package:aula_arquitetura/module/dashboard/data/repository/listar_pessoas_repository.dart';
-import 'package:aula_arquitetura/module/dashboard/di/controller.dart';
 import 'package:aula_arquitetura/module/manutencao_dashboard/view/manutencao_dashboard.dart';
-import 'package:aula_arquitetura/util/database.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class DashboardView extends StatefulWidget {
-  const DashboardView({super.key});
+class DashboardView extends StatelessWidget {
+  final controller = getIt<DashboardController>();
 
-  @override
-  State<DashboardView> createState() => _DashboardViewState();
-}
-
-class _DashboardViewState extends State<DashboardView> {
-  late DashboardController dashboardController;
-
-  @override
-  initState() {
-    super.initState();
-    dashboardController = DashboardControllerDI.getInstance(setState: setState);
-
-    dashboardController.listarPessoas();
-  }
+  DashboardView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: dashboardController.loading
-          ? Center(child: CircularProgressIndicator())
-          : ListView.builder(
-              itemBuilder: (context, index) {
-                return Text(dashboardController.pessoas[index].nome);
+    return BlocProvider(
+      create: (context) => controller,
+      child: BlocBuilder<DashboardController, DashboardState>(
+        builder: (context, state) {
+          return Scaffold(
+            body: Builder(
+              builder: (context) {
+                if (state is DashboardLoading) {
+                  return Center(child: CircularProgressIndicator());
+                } else if (state is DashboardLoaded) {
+                  return ListView.builder(
+                    itemBuilder: (context, index) {
+                      return Text(state.pessoas[index].nome);
+                    },
+                    itemCount: state.pessoas.length,
+                  );
+                }
+                return Center(child: Text('Estado inicial'));
               },
-              itemCount: dashboardController.pessoas.length,
             ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          final pessoa = await Navigator.push<Pessoa>(
-            context,
-            MaterialPageRoute(builder: (_) => ManutencaoDashboard()),
-          );
+            floatingActionButton: FloatingActionButton(
+              onPressed: () async {
+                final pessoa = await Navigator.push<Pessoa>(
+                  context,
+                  MaterialPageRoute(builder: (_) => ManutencaoDashboard()),
+                );
 
-          if (pessoa != null) {
-            dashboardController.adicionarPessoa(pessoa);
-          }
+                if (pessoa != null && context.mounted) {
+                  controller.adicionarPessoa(pessoa);
+                }
+              },
+              child: Icon(Icons.add),
+            ),
+          );
         },
-        child: Icon(Icons.add),
       ),
     );
   }
